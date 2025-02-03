bfcec7b6344c5a24bb3e6fb673491a549a07f1c7
pledp <pred.lers@gmail.com>
2025-02-03 23:19:06 +0200

    import bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Hero from "@/components/Hero";
+import Hero from "@/components/hero";
 import Projects from "@/components/projects"
 import StaggeredGrid from "@/components/dot-grid-component";
 import Footer from "@/components/footer";
